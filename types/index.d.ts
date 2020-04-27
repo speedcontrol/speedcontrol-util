3b90a230cfd5462777aa3ed297b8036403f9baf4
zoton2 <zoton2@gmail.com>
2020-04-27 21:48:22 +0100

    Actually use .gitignore properly and include export for types

---
+++ types/index.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable object-curly-newline */
+/* eslint-disable max-len */
+
+export { RunDataActiveRunSurrounding, TimerChangesDisabled, TwitchCommercialTimer } from 'nodecg-speedcontrol/schemas';
+export { CommercialDuration, ExtensionReturn, ListenFor, RunData, RunDataActiveRun, RunDataArray, RunFinishTimes, SendMessage, SendMessageReturnMap, Timer } from 'nodecg-speedcontrol/types';
+
