820c20bc260663c6fedf30cc47aaa0f565000ac1
zoton2 <zoton2@gmail.com>
2020-07-16 10:43:20 +0100

    Expore more types I needed for my own need

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,5 +3,5 @@
 /* eslint-disable max-len */
 
 export { RunDataActiveRunSurrounding, TimerChangesDisabled, TwitchCommercialTimer } from 'nodecg-speedcontrol/schemas';
-export { CommercialDuration, ExtensionReturn, ListenFor, RunData, RunDataActiveRun, RunDataArray, RunFinishTimes, SendMessage, SendMessageReturnMap, Timer } from 'nodecg-speedcontrol/types';
+export { CommercialDuration, ExtensionReturn, ListenFor, RunData, RunDataActiveRun, RunDataArray, RunDataPlayer, RunDataTeam, RunFinishTimes, SendMessage, SendMessageReturnMap, TeamFinishTime, Timer } from 'nodecg-speedcontrol/types';
 
