5a5fbbcdcc0bc48a67df75bbd940b06d9da28fdb
zoton2 <zoton2@gmail.com>
2021-07-08 15:52:49 +0100

    Update deps, update/fix some broken types

--- types/speedcontrol/schemas/runDataActiveRun.d.ts
+++ types/speedcontrol/schemas/runDataActiveRun.d.ts
@@ -18,9 +18,11 @@ export type RunDataActiveRun = {
 	setupTimeS?: number;
 	scheduled?: string;
 	scheduledS?: number;
+	relay?: boolean;
 	teams: {
 		name?: string;
 		id: string;
+		relayPlayerID?: string;
 		players: {
 			name: string;
 			id: string;
